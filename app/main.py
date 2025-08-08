674a3d2effa3ddb039b45fcb5868b80eff40065c
LuisGonzalez-Dilato <luis.gonzalez@dilatoinfotech.com>
2025-08-08 12:34:53 -0700

    Adding new tests

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 def say_hello():
     return "Hello, World! Testing that automation test works."
 
+def new_test():
+    return "New automation test."
+
 if __name__ == "__main__":
     print(say_hello())
+    print(new_test())
+    input("Press any key to exit.")
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,7 @@
-from app.main import say_hello
+from app.main import say_hello, new_test
 
 def test_say_hello():
     assert say_hello() == "Hello, World! Testing that automation test works."
+
+def test_new_test():
+    assert new_test() == "New automation test."
