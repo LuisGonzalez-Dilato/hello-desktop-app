648ebb3ab64c528e48c803044938a027f130f3f9
LuisGonzalez-Dilato <luis.gonzalez@dilatoinfotech.com>
2025-08-08 09:52:39 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
 def say_hello():
-    return "Hello, World! I have fixed the fucking tests"
+    return "Hello, World!"
 
 if __name__ == "__main__":
     print(say_hello())
-    input("Press Enter to exit the fucking fixes...")
-
